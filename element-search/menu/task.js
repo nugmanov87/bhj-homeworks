e941b26b45e7f0b9438018de36e6fed37043fe53
Ruslan Nugmanov <49123257+nugmanov87@users.noreply.github.com>
2019-10-22 16:10:42 +0500

    2 версия

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -2,22 +2,20 @@
 const menuLink = document.getElementsByClassName("menu__link");
 const menuSub = document.querySelectorAll("ul.menu_sub");
 
+function removeActive() {
+  for (let i = 0; i < menuSub.length; i++) {
+    menuSub[i].classList.remove("menu_active");
+  }
+}
+
 for (let i = 0; i < menuLink.length; i++) {
   menuLink[i].onclick = function() {
-    if (menuLink[i].nextElementSibling == menuSub[0]) {
-      menuSub[0].className = "menu menu_sub menu_active";
-      if ((menuSub[1].className = "menu menu_sub menu_active")) {
-        menuSub[1].className = "menu menu_sub";
-      }
-      return false;
-    }
+    removeActive();
 
-    if (menuLink[i].nextElementSibling == menuSub[1]) {
-      menuSub[1].className = "menu menu_sub menu_active";
-      if ((menuSub[0].className = "menu menu_sub menu_active")) {
-        menuSub[0].className = "menu menu_sub";
-      }
+    if (menuLink[i].nextElementSibling.classList.contains("menu_sub")) {
+      menuLink[i].nextElementSibling.classList.add("menu_active");
       return false;
     }
   };
 }
+
