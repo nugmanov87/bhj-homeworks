0c560417d9578b9ca994e24d3eaaee9df6943fe9
Ruslan <ruslan-nugmanov87@mail.ru>
2019-10-23 21:31:50 +0500

    3 версия

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -2,21 +2,24 @@
 const menuLink = document.getElementsByClassName("menu__link");
 const menuSub = document.querySelectorAll("ul.menu_sub");
 
+function removeActive() {
+  for (let i = 0; i < menuSub.length; i++) {
+    menuSub[i].classList.remove("menu_active");
+  }
+}
+
 for (let i = 0; i < menuLink.length; i++) {
   menuLink[i].onclick = function() {
-    if (menuLink[i].nextElementSibling == menuSub[0]) {
-      menuSub[0].className = "menu menu_sub menu_active";
-      if ((menuSub[1].className = "menu menu_sub menu_active")) {
-        menuSub[1].className = "menu menu_sub";
-      }
+    const element = menuLink[i].nextElementSibling.classList;
+
+    if (element.contains("menu_active")) {
+      removeActive();
       return false;
     }
 
-    if (menuLink[i].nextElementSibling == menuSub[1]) {
-      menuSub[1].className = "menu menu_sub menu_active";
-      if ((menuSub[0].className = "menu menu_sub menu_active")) {
-        menuSub[0].className = "menu menu_sub";
-      }
+    if (element.contains("menu_sub")) {
+      removeActive();
+      element.add("menu_active");
       return false;
     }
   };
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,3 +1,4 @@
+"use srtict";
 const prevButton = document.querySelector(".slider__arrow_prev");
 const nextButton = document.querySelector(".slider__arrow_next");
 
@@ -6,36 +7,30 @@ let slideNumber = 0;
 const sliderImages = document.getElementsByClassName("slider__item");
 let sliderImage = sliderImages.item(slideNumber);
 
+function showSlides(n) {
+  sliderImage = sliderImages.item(slideNumber + n);
+  sliderImage.classList.remove("slider__item_active");
+  sliderImage = sliderImages.item(slideNumber);
+  sliderImage.classList.add("slider__item_active");
+}
+
 nextButton.onclick = function() {
   if (slideNumber >= 0 && slideNumber < sliderImages.length - 1) {
     slideNumber++;
-    sliderImage = sliderImages.item(slideNumber - 1);
-    sliderImage.classList.remove("slider__item_active");
-    sliderImage = sliderImages.item(slideNumber);
-    sliderImage.classList.add("slider__item_active");
+    showSlides(-1);
   } else {
     slideNumber = 0;
-    sliderImage = sliderImages.item(slideNumber);
-    sliderImage.classList.add("slider__item_active");
-    for (let i = 1; i < 5; i++) {
-      sliderImage = sliderImages.item(slideNumber + i);
-      sliderImage.classList.remove("slider__item_active");
-    }
+    sliderImage.classList.remove("slider__item_active");
   }
 };
 
 prevButton.onclick = function() {
   if (slideNumber < sliderImages.length && slideNumber > 0) {
     slideNumber--;
-    sliderImage = sliderImages.item(slideNumber + 1);
-    sliderImage.classList.remove("slider__item_active");
-    sliderImage = sliderImages.item(slideNumber);
-    sliderImage.classList.add("slider__item_active");
+    showSlides(1);
   } else {
     slideNumber = sliderImages.length - 1;
     sliderImage = sliderImages.item(0);
-    sliderImage.classList.remove("slider__item_active");
-    sliderImage = sliderImages.item(slideNumber);
-    sliderImage.classList.add("slider__item_active");
+    showSlides();
   }
 };
